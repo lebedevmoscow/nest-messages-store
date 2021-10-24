32ed4a23a85589f72283af25d8f472be6b1d4690
Boris Lebedev <lebedev.moscow.me@gmail.com>
2021-10-24 07:46:44 -0700

    updates

--- src/messages/messages.controller.ts
+++ src/messages/messages.controller.ts
@@ -1,13 +1,33 @@
-import { Controller, Get, Post } from "@nestjs/common"
+import { Controller, Get, Post, Body, Param, NotFoundException } from "@nestjs/common"
+import { CreateMessageDTO } from "./dtos/create-message.dto"
+import { MessagesService } from "./messages.service"
 
 @Controller("messages")
 export class MessagesController {
+    messagesService: MessagesService
+
+    constructor() {
+        this.messagesService = new MessagesService()
+    }
+
     @Get()
-    listMessages() {}
+    listMessages() {
+        return this.messagesService.findAll()
+    }
 
     @Post()
-    createMessage() {}
+    createMessage(@Body() body: CreateMessageDTO) {
+        return this.messagesService.create(body.content)
+    }
 
     @Get("/:id")
-    getMessage() {}
+    async getMessage(@Param("id") id: string) {
+        const message = await this.messagesService.findOne(id)
+
+        if (!message) {
+            throw new NotFoundException("Message not found")
+        }
+
+        return message
+    }
 }
